b65fd09e3fc50c137556278b1f356d64c8583379
Jacob Torres <jacob@jacobtorres.net>
2021-08-20 15:27:25 -0600

    Authorization successful in auth.py.

--- app/auth.py
+++ app/auth.py
@@ -1,9 +1,14 @@
 # Authorizes the application to access the Spotify API
 import os
 from dotenv import load_dotenv
+from spotipy import Spotify
+from spotipy.oauth2 import SpotifyClientCredentials
 
 # Get environment variables
 load_dotenv()
 CLIENT_ID = os.getenv('CLIENT_ID')
 CLIENT_SECRET = os.getenv('CLIENT_SECRET')
 
+# Authorize via spotipy
+auth_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+sp = Spotify(auth_manager=auth_manager)
