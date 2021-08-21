598270744e3a34b9e2b5d4516a53f4911fe08255
Jacob Torres <jacob@jacobtorres.net>
2021-08-20 19:24:47 -0600

    Foundation for requests authorization in auth.py; not yet functional.

--- app/auth.py
+++ app/auth.py
@@ -1,14 +1,47 @@
-# Authorizes the application to access the Spotify API
 import os
+import requests
+from requests.api import request
 from dotenv import load_dotenv
-from spotipy import Spotify
-from spotipy.oauth2 import SpotifyClientCredentials
 
-# Get environment variables
 load_dotenv()
+AUTH_URL = 'https://accounts.spotify.com/authorize'
+API_URL = 'https://accounts.spotify.com/api/token'
+SCOPE = ['user-library-read', 'playlist-read-private']
 CLIENT_ID = os.getenv('CLIENT_ID')
 CLIENT_SECRET = os.getenv('CLIENT_SECRET')
+REDIRECT_URI = os.getenv('REDIRECT_URI')
 
-# Authorize via spotipy
-auth_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
-sp = Spotify(auth_manager=auth_manager)
+
+def authorize():
+    """
+    Authorizes the app to access the spotify API.
+    returns: authorized api object
+    """
+
+    try:
+        res = requests.request(
+            method='get',
+            url=AUTH_URL,
+            params={
+                'client_id': CLIENT_ID,
+                'response_type': 'code',
+                'redirect_uri': REDIRECT_URI,
+                'scope': SCOPE
+            }
+        )
+
+        if res.ok == True:
+            AUTH_CODE = res.text
+
+            res2 = requests.request(
+                'method': 'post',
+                'url': API_URL,
+                params={
+                    'grant_type': 'authorization_code',
+                    'code': AUTH_CODE,
+                    'redirect_uri': REDIRECT_URI
+                },
+                headers={
+                    'Authorization': CLIENT_ID + ':' + CLIENT_SECRET
+                }
+            )
