da3bf95db33593cc1b8e85e1d71027312918457b
Jacob Torres <jacob@jacobtorres.net>
2021-08-23 17:45:29 -0600

    Authorize function in user_auth.py is working; Spotipy package implemented.

--- app/user_auth.py
+++ app/user_auth.py
@@ -1,12 +1,21 @@
 import os
-import requests
-from requests.api import request
 from dotenv import load_dotenv
+from spotipy import Spotify
+from spotipy.oauth2 import SpotifyOAuth
 
 load_dotenv()
 AUTH_URL = 'https://accounts.spotify.com/authorize'
 API_URL = 'https://accounts.spotify.com/api/token'
-SCOPE = ['user-library-read', 'playlist-read-private']
+SCOPE = [
+    'playlist-read-private',
+    'playlist-read-collaborative',
+    'user-read-private',
+    'user-library-read',
+    'user-read-currently-playing',
+    'user-read-recently-played',
+    'user-top-read'
+]
+
 CLIENT_ID = os.getenv('CLIENT_ID')
 CLIENT_SECRET = os.getenv('CLIENT_SECRET')
 REDIRECT_URI = os.getenv('REDIRECT_URI')
@@ -14,34 +23,21 @@ REDIRECT_URI = os.getenv('REDIRECT_URI')
 
 def authorize():
     """
-    Authorizes the app to access the spotify API.
-    returns: authorized api object
+    Authorizes the app to access the spotify API using Spotipy.
+    returns: <spotipy.client.Spotify> -- Authorized Spotify API object
     """
 
     try:
-        res = requests.request(
-            method='get',
-            url=AUTH_URL,
-            params={
-                'client_id': CLIENT_ID,
-                'response_type': 'code',
-                'redirect_uri': REDIRECT_URI,
-                'scope': SCOPE
-            }
+        auth_manager = SpotifyOAuth(
+            client_id=CLIENT_ID,
+            client_secret=CLIENT_SECRET,
+            redirect_uri=REDIRECT_URI,
+            scope=SCOPE
         )
 
-        if res.ok == True:
-            AUTH_CODE = res.text
+        sp = Spotify(auth_manager=auth_manager)
 
-            res2 = requests.request(
-                'method': 'post',
-                'url': API_URL,
-                params={
-                    'grant_type': 'authorization_code',
-                    'code': AUTH_CODE,
-                    'redirect_uri': REDIRECT_URI
-                },
-                headers={
-                    'Authorization': CLIENT_ID + ':' + CLIENT_SECRET
-                }
-            )
+        return sp
+
+    except Exception as err:
+        print(err)
