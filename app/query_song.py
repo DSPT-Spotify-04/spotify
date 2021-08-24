a058cce835c87c73c39fbe1af864d869e88616e1
ubaidurk <77473624+ubaidurk@users.noreply.github.com>
2021-08-23 23:14:39 -0400

    Update query_song.py

--- app/query_song.py
+++ app/query_song.py
@@ -1,24 +1,30 @@
 import requests
 import os
 from requests.api import request
+import json
 
 '''Use the Spotify API to search for a song by name, to get the song ID.'''
 '''We can then use the song ID to get audio features for the song'''
 
-track_name = "Hello" # Sample Song
-API_KEY = os.getenv('API_KEY') # Sample: "Bearer BQAX9-FISGNKN
-                               # bAt3f40adE3zv3k9IhdoGdR59duunm
-                               # lXkasdeqvFrvcOAqHOoI5o5XQA1fSo
-                               # C4eEuovVE8m9oFl6kIcAaB7B8L3poOc
-                               # crIH4ZbpjXgPbLBFONrzPqVrU7ZM59X
-                               # gBGzwVjx8pBXH2Z_cMYGxdt4"
+API_KEY = os.getenv('API_KEY')
 
-url = str('https://api.spotify.com/v1/search?q=' + track_name + '&type=track&limit=5')
+def get_song_id_by_name(track_name):
+    url = str('https://api.spotify.com/v1/search?q=' + track_name + '&type=track&limit=5')
 
-headers = {"Accept": "application/json",
-           "Content-Type": "application/json",
-           "Authorization": API_KEY}
+    headers = {"Accept": "application/json",
+               "Content-Type": "application/json",
+               "Authorization": "Bearer BQCel627383_kzC7sfoH548qhL7ZCicQRHaV0DzcLjVTa_lhqHWRWpVbhpAfvgyrRgkflDKaBd01YD8BsQ8rRayIgkNNV5ahzn0A3hxHzIiUbMS5PM9x9o_QhjhyUM77Q8g979iw_4twyMSuOHFqZLjrEBEVrsA"}
 
-r = requests.get(url, headers=headers)
+    r = requests.get(url, headers=headers)
 
-print(r.text)
+    jsondata = json.loads(r.text)
+
+    song_ids = []
+
+    for i in range(len(jsondata['tracks']['items'])):
+        if jsondata['tracks']['items'][i]['album']['album_type'] == 'single':
+            song_ids.append(jsondata['tracks']['items'][i]['id'])
+
+    song = song_ids[0]
+
+    print(song)
