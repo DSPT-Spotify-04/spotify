93e4237651fef070e07925aac2f8fbc59e6d663f
ubaidurk <77473624+ubaidurk@users.noreply.github.com>
2021-08-23 20:28:42 -0400

    Update query_song.py

--- app/query_song.py
+++ app/query_song.py
@@ -6,7 +6,12 @@ from requests.api import request
 '''We can then use the song ID to get audio features for the song'''
 
 track_name = "Hello" # Sample Song
-API_KEY = os.getenv('API_KEY') # Sample: "Bearer BQAX9-FISGNKNbAt3f40adE3zv3k9IhdoGdR59duunmlXkasdeqvFrvcOAqHOoI5o5XQA1fSoC4eEuovVE8m9oFl6kIcAaB7B8L3poOccrIH4ZbpjXgPbLBFONrzPqVrU7ZM59XgBGzwVjx8pBXH2Z_cMYGxdt4"
+API_KEY = os.getenv('API_KEY') # Sample: "Bearer BQAX9-FISGNKN
+                               # bAt3f40adE3zv3k9IhdoGdR59duunm
+                               # lXkasdeqvFrvcOAqHOoI5o5XQA1fSo
+                               # C4eEuovVE8m9oFl6kIcAaB7B8L3poOc
+                               # crIH4ZbpjXgPbLBFONrzPqVrU7ZM59X
+                               # gBGzwVjx8pBXH2Z_cMYGxdt4"
 
 url = str('https://api.spotify.com/v1/search?q=' + track_name + '&type=track&limit=5')
 
