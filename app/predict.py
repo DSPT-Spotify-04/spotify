57c721074dd77ee9ea5b978a37124139459f931b
ubaidurk <77473624+ubaidurk@users.noreply.github.com>
2021-08-24 16:55:36 -0400

    Update predict.py

--- app/predict.py
+++ app/predict.py
@@ -7,10 +7,22 @@ import sqlite3
 con = sqlite3.connect("spotify_db.sqlite3")
 cur = con.cursor()
 master_song_db = cur.execute("SELECT * FROM data;")
+user_selected_songs_db = cur.execute("SELECT * FROM Song;")
 
 target = 'is_recommended'
 
-train = pd.DataFrame(Song.query.all())
+train = pd.DataFrame(user_selected_songs_db, columns=[
+    'id', 'name', 'energy', 'key', 'loudness', 'mode',
+    'speechiness', 'acousticness', 'instrumentalness',
+    'liveness', 'valence', 'tempo'
+])
+
+train = train[[ # Reordering columns to match testing data
+    'name', 'id', 'acousticness',
+    'energy', 'tempo', 'instrumentalness', 'key', 'liveness',
+    'loudness', 'mode', 'valence', 'speechiness']]
+
+train['is_recommended'] = 1
 
 test = pd.DataFrame(master_song_db, columns=[
     'name', 'id', 'acousticness', 'danceability', 'duration_ms',
@@ -19,10 +31,10 @@ test = pd.DataFrame(master_song_db, columns=[
 ])
 test['is_recommended'] = pd.Series()
 
-X_test = test.drop(columns=[target, 'duration_ms'])
-y_test = test[target]
-X_train = train.drop(columns=target)
+X_train = train.drop(columns=[target, 'name', 'id'])
 y_train = train[target]
+X_test = test.drop(columns=[target, 'duration_ms', 'danceability']) # Drop target and also the columns we don't have in our training data
+y_test = test[target]
 
 model = LinearRegression()
 
