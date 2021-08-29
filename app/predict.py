835448e4e021ce52692c549c24bec150bde64ca9
MicroCapacitor <79718621+MicroCapacitor@users.noreply.github.com>
2021-08-28 23:51:27 -0500

    Update predict.py
    Add predict_songs() function to predict top 10 songs that the user would enjoy.

--- app/predict.py
+++ app/predict.py
@@ -12,47 +12,54 @@ data = [  # Data columns
     'energy', 'tempo', 'instrumentalness', 'key', 'liveness',
     'loudness', 'mode', 'valence', 'speechiness']
 
-user_selected_songs_db = cur.execute("SELECT * FROM Song;")  # All of the songs entered in by the user through the app
 
-train = pd.DataFrame(user_selected_songs_db, columns=[
-    # Renaming the columns, because importing the sqlite3 database simply names them '1...2...3...4...'
-    'id', 'name', 'energy', 'key', 'loudness', 'mode',
-    'speechiness', 'acousticness', 'instrumentalness',
-    'liveness', 'valence', 'tempo'
-])
+def predict_songs():
+    # All of the songs entered in by the user through the app
+    user_selected_songs_db = cur.execute("SELECT * FROM Song;")
 
-train = train[[  # Reordering columns to match testing data
-    'name', 'id', 'acousticness',
-    'energy', 'tempo', 'instrumentalness', 'key', 'liveness',
-    'loudness', 'mode', 'valence', 'speechiness']]
+    train = pd.DataFrame(user_selected_songs_db, columns=[
+        # Renaming the columns, because importing the sqlite3 database simply names them '1...2...3...4...'
+        'id', 'name', 'energy', 'key', 'loudness', 'mode',
+        'speechiness', 'acousticness', 'instrumentalness',
+        'liveness', 'valence', 'tempo'
+    ])
 
-train[
-    'is_recommended'] = 1  # The songs that the user has entered in *are* recommended. We train a model using their
-# songs, then predict "is_recommended" for the big dataset with 160K songs (test)
+    # Reordering columns to match testing data
+    train = train[[
+        'name', 'id', 'acousticness',
+        'energy', 'tempo', 'instrumentalness', 'key', 'liveness',
+        'loudness', 'mode', 'valence', 'speechiness']]
+    # The songs that the user has entered in *are* recommended. We train a model using their
+    # songs, then predict "is_recommended" for the big dataset with 160K songs (test)
+    train['is_recommended'] = 1
 
+    # 'data' is a table with 160K existing songs
+    master_song_db = cur.execute("SELECT * FROM data;")
 
-master_song_db = cur.execute("SELECT * FROM data;")  # 'data' is a table with 160K existing songs
+    test = pd.DataFrame(master_song_db, columns=[  # Renaming columns
+        'name', 'id', 'acousticness', 'danceability', 'duration_ms',
+        'energy', 'tempo', 'instrumentalness', 'key', 'liveness',
+        'loudness', 'mode', 'valence', 'speechiness'
+    ])
+    test['is_recommended'] = 0
 
-test = pd.DataFrame(master_song_db, columns=[  # Renaming columns
-    'name', 'id', 'acousticness', 'danceability', 'duration_ms',
-    'energy', 'tempo', 'instrumentalness', 'key', 'liveness',
-    'loudness', 'mode', 'valence', 'speechiness'
-])
-test['is_recommended'] = 1
+    # Get the X/Y sets for both "train" and "test"
+    X_train = train[data]
+    y_train = train[target]
+    X_test = test[data]
+    y_test = test[target]
 
-# Get the X/Y sets for both "train" and "test"
-X_train = train[data]
-y_train = train[target]
-X_test = test[data]
-y_test = test[target]
+    # Create model
+    model = LinearRegression()
+    model.fit(X=X_train, y=y_train)
 
-model = LinearRegression()
+    counter = 0
+    songratings = []
+    for index, row in X_test.iterrows():
+        output = model.predict([row])
+        songratings.append((test['id'][counter], output))
 
-model.fit(X=X_train, y=y_train)
-
-
-for index, row in X_test.iterrows():
-    output = model.predict([row])
-    print(output)
-    break
-print(model.score(X_test, y_test))
+        print(output)
+        counter += 1
+        break
+    print(model.score(X_test, y_test))
